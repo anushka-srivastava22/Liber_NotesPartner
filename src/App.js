b2777eb2cad794b846e8469fa96b902cd497e1e9
SarthakKeshari <srthkkeshari84@gmail.com>
2022-03-09 15:21:51 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './Helpers/Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Helpers/Navbar";
 
 function App() {
   return (
     // Footer
     <Navbar id="footer">
-      Hello
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 col-md-4 col-sm-12">
+            <img src="./logo.png" className="img-fluid"/>
+          </div>  
+          <div className="col-lg-8 col-md-8 col-sm-12">
+            Hello
+          </div>  
+        </div>
+        
+        &copy;<small>Sarthak Keshari and Anushka Srivastava. All rights reserved.</small>
+      </div>
     </Navbar>
   );
 }
